5d32ac1b4024bccbf290ed66b9548edbe1de5b46
Scott Hutley <scotthutley1@comcast.net>
2021-10-15 16:32:03 -0700

    Add Create and Details machine views w/ cshtml

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -20,5 +20,30 @@ namespace Factory.Controllers
       List<Machine> model = _db.Machines.ToList();
       return View(model);
     }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Machine machine)
+    {
+      _db.Machines.Add(machine);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    
+    public ActionResult Details(int id)
+    {
+      var thisMachine = _db.Machines
+          .Include(machine => machine.JoinEntities)
+          .ThenInclude(join => join.Engineer)
+          .FirstOrDefault(machine => machine.MachineId == id);
+      return View(thisMachine);
+    }
+
+    
+
   }
 }
